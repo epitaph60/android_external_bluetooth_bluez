e2b14caadf56e56e728557bbab9302a84bc05fe2
Jaikumar Ganesh <jaikumar@google.com>
2009-06-11 13:37:55 -0700

    Add capabilities to bluetoothd.
    As android init.rc doesn't support applying linux capabilities,
    add them after starting the bluetooth daemon as root.

---
+++ common/android_bluez.c
@@ -0,0 +1,43 @@
+/*
+ *
+ *  BlueZ - Bluetooth protocol stack for Linux
+ *
+ *  Copyright (C) 2004-2009  Marcel Holtmann <marcel@holtmann.org>
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include <private/android_filesystem_config.h>
+#include <sys/prctl.h>
+#include <linux/capability.h>
+
+/* Set UID to bluetooth w/ CAP_NET_RAW, CAP_NET_ADMIN and CAP_NET_BIND_SERVICE
+ * (Android's init.rc does not yet support applying linux capabilities) */
+void android_set_aid_and_cap() {
+	prctl(PR_SET_KEEPCAPS, 1, 0, 0, 0);
+	setuid(AID_BLUETOOTH);
+
+	struct __user_cap_header_struct header;
+	struct __user_cap_data_struct cap;
+	header.version = _LINUX_CAPABILITY_VERSION;
+	header.pid = 0;
+	cap.effective = cap.permitted = 1 << CAP_NET_RAW |
+					1 << CAP_NET_ADMIN |
+					1 << CAP_NET_BIND_SERVICE;
+	cap.inheritable = 0;
+	capset(&header, &cap);
+}
